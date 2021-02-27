99590cf94c8d279166d6b70441c2f9ecf0ff79a9
Allencs16 <brenoallencs@gmail.com>
2021-02-27 13:10:00 -0300

    First Day

--- src/server.ts
+++ src/server.ts
@@ -6,4 +6,8 @@ app.get ("/", (request, response)  => {
   return response.json({ message: "Tudo certo, servidor operando no status 200" });
 });
 
+app.post ("/", (request, response)  => {
+  return response.json({ message: "Salvo com sucesso!" });
+});
+
 app.listen(3000, () => console.log("Server is Running!"));
